abd2cb341a04172046618d0085dc2050ecf9a25b
phuc181003 <phuc07860@gmail.com>
2024-12-16 21:24:26 +0700

    fix buildmode dev and buildmode production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 // export const API_ROOT = 'http://localhost:8017/api/v1'
-export const API_ROOT = 'https://trelloapp-server.onrender.com/api/v1'
+
+let api_root = ''
+if (process.env.BUILD_MODE === 'dev') {
+  api_root = 'http://localhost:8017/api/v1'
+}
+if (process.env.BUILD_MODE === 'production') {
+  api_root = 'https://trelloapp-server.onrender.com/api/v1'
+}
+export const API_ROOT = api_root
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // cho phép Vite sử dụng được process.env, mặc định thì không mà phải sử dụng thằng import.meta.env
+  // https://github.com/vitejs/vite/issues...
+  define: {
+    'process.env': process.env
+  },
   plugins: [react()],
   resolve: {
     alias: [
